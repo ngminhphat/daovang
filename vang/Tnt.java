46d33338ac34d74d642d9148910c69860e448610
nkien1424 <132690996+nkien1424@users.noreply.github.com>
2024-04-20 16:51:20 +0700

    Create Tnt.java

---
+++ vang/Tnt.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.dao.vang;
+
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Tnt extends Object {
+    
+    boolean biKeo = false;
+    BufferedImage image;
+    int weight;
+    static int count=0;
+    public Tnt(int posX,int posY) {
+        super(posX,posY);        
+        this.weight= weight;
+        try {
+            image = ImageIO.read(new File("Resources/tnt1.png"));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        count++;
+    }
+
+    @Override
+    public void draw(Image bufferedImage) {
+        Graphics g = bufferedImage.getGraphics();
+        g.drawImage(image, this.get_x(), this.get_y(), null);
+    }
+    
+}

88181dc3804ad08916d20fc11e468144191a8185
nkien1424 <132690996+nkien1424@users.noreply.github.com>
2024-04-21 15:46:35 +0700

    Create Da.java

---
+++ vang/Da.java
@@ -0,0 +1,33 @@
+package com.mycompany.dao.vang;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+
+public class Da extends Object {
+	int score;
+	int weight;
+        boolean biKeo = false;
+	BufferedImage image;
+        
+	public Da(int posX, int posY, String img, int score, int weight) {
+		super(posX, posY);
+		this.score = score;
+		this.weight = weight;
+		try {
+			image = ImageIO.read(new File(img));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void draw(Image bufferedImage) {
+		Graphics g = bufferedImage.getGraphics();
+		g.drawImage(this.image, this.get_x(), this.get_y(), null);
+	}
+	
+	
+}

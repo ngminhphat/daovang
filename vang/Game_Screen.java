f51699ff952ba5e7d9908f164f79048edb2ab1cb
nkien1424 <132690996+nkien1424@users.noreply.github.com>
2024-04-22 15:34:04 +0700

    Update Game_Screen.java

--- vang/Game_Screen.java
+++ vang/Game_Screen.java
@@ -14,9 +14,16 @@ import javax.imageio.ImageIO;
  */
 public class Game_Screen implements Screen {
 	BufferedImage bg, bg2, bufferedImage2;
+        int count = 0;
+        int count2 = 0;
+        int tempScore;
+        int scoreX = 80;
+        int timeChuc, timeDonVi;
+        int dem =30;
+        int targetScore = 500;
 	Player player;
 	public ArrayList<Gold> listGold = new ArrayList<>();
-	public ArrayList<Image> listTime = new ArrayList<>();
+	public ArrayList<Image> listDiem  = new ArrayList<>();
 	public ArrayList<Da> listDa = new ArrayList<>();
 	public ArrayList<Pig> listPig = new ArrayList<>();
 	public ArrayList<Tnt> listTnt = new ArrayList<>();
@@ -24,12 +31,15 @@ public class Game_Screen implements Screen {
 	Gold g1, g2, g3, g4, g5, g6, g7;
 	Da d1, d2;
 	Pig pig1;
-	Tnt tnt;
-
-	public Game_Screen() {
+	Tnt tnt,tnt1;
+        kim_cuong kc;
+        public ArrayList<kim_cuong> listKc = new ArrayList<>();
+	public Game_Screen( ) {
 		player = new Player(300, 24);
+                listDiem = new ArrayList<>();
+               
 		try {
-			g1 = new Gold(90, 390, "Resources/vang_1.png", 400, 1);
+			g1 = new Gold(50, 390, "Resources/vang_1.png", 400, 1);
 			g2 = new Gold(600, 320, "Resources/vang_3.png", 300, 3);
 			g3 = new Gold(430, 430, "Resources/vang_3.png", 100, 3);
 			g4 = new Gold(200, 350, "Resources/vang_4.png", 50, 4);
@@ -37,12 +47,27 @@ public class Game_Screen implements Screen {
 			g6 = new Gold(450, 300, "Resources/vang_4.png", 50, 4);
 			g7 = new Gold(130, 250, "Resources/vang_4.png", 50, 4);
 			d1 = new Da(250, 390, "Resources/da_1.png", 20, 1);
-			d2 = new Da(660, 250, "Resources/da_2.png", 15, 2);
+			d2 = new Da(660, 250, "Resources/da_1.png", 15, 1);
 			pig1 = new Pig(300, 520, "Resources/pig.png", 501, 4);
 			tnt = new Tnt(350, 290);
+                        kc = new kim_cuong(650,400,"Resources/kc.png",500);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+                try {
+                        listDiem.add(ImageIO.read(new File("Resources/so0.png")));
+                        listDiem.add(ImageIO.read(new File("Resources/so1.png")));
+                        listDiem.add(ImageIO.read(new File("Resources/so2.png")));
+                        listDiem.add(ImageIO.read(new File("Resources/so3.png")));
+                        listDiem.add(ImageIO.read(new File("Resources/so4.png")));
+                        listDiem.add(ImageIO.read(new File("Resources/so5.png")));
+                        listDiem.add(ImageIO.read(new File("Resources/so6.png")));
+                        listDiem.add(ImageIO.read(new File("Resources/so7.png")));
+                        listDiem.add(ImageIO.read(new File("Resources/so8.png")));
+                        listDiem.add(ImageIO.read(new File("Resources/so9.png")));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
 		listGold.add(g1);
 		listGold.add(g2);
 		listGold.add(g3);
@@ -54,33 +79,48 @@ public class Game_Screen implements Screen {
 		listDa.add(d2);
 		listPig.add(pig1);
 		listTnt.add(tnt);
+                listKc.add(kc);
 		try {
 			bg = ImageIO.read(new File("Resources/bg.png"));
 			bg2 = ImageIO.read(new File("Resources/bg2.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 	}
 
 	@Override
 	public void click() {
 		
-		Game_Manager.getInstance().getStackScreen().push(new Screen_nextlevel());
+            Game_Manager.getInstance().getStackScreen().push(new game_over());
 	}
-
+//        public void next_man() {
+//            Game_Manager.getInstance().getStackScreen().push(new Screen_nextlevel());
+//        }
 	@Override
 	public void update() {
 		
 		Player.moc.an_vang(listGold);
 		Player.moc.anTnt(listTnt);
                 Player.moc.anDa(listDa);
-                Player.moc.anPig(listPig);
-                
+                Player.moc.anPig(listPig); 
+                Player.moc.anKc(listKc);
 		Player.moc.update();
                 for (Pig pig : listPig) {
 			pig.update();
 		}
+                count++;
+                if(count2 == 59){
+                    dem--;
+                    count2 = 0;
+                }
+                count2++;
+                tempScore = player.moc.tongDiem;
+//                if( dem ==0 && (tempScore >=targetScore )) {
+//                    this.next_man();
+//                }
+                if( dem==0 &&(tempScore <targetScore )) {
+                    this.click();
+                }
 		if (player.moc.keoVe) {
 			Iterator itr1 = listGold.iterator();
 			while (itr1.hasNext()) {
@@ -109,15 +149,18 @@ public class Game_Screen implements Screen {
             while (itr7.hasNext()) {
                 Tnt s = (Tnt) itr7.next();
                 if (s.biKeo) {
-                    itr7.remove();
-                    s.draw1(bufferedImage2,350,24);
+                    itr7.remove();                        
                 }
-
             }
-
-		}
-
-	}
+            Iterator itr8 = listKc.iterator();
+            while (itr7.hasNext()) {
+                kim_cuong s = (kim_cuong) itr7.next();
+                if (s.biKeo) {
+                    itr7.remove();                        
+                }
+            }
+        }
+    }
 
 	public void gapDo() {
 		player.moc.speedY = 3;
@@ -149,7 +192,17 @@ public class Game_Screen implements Screen {
 		bufferGraphic2D.drawImage(bg, 0, 150, null);
 		player.draw(bufferedImage2);
 		Player.moc.draw(bufferedImage2);
-
+                tempScore = player.moc.tongDiem;
+                scoreX = 80;
+                for(int i = 0; i < 4; i++){
+                    bufferGraphic2D.drawImage(listDiem.get(tempScore%10), scoreX, 50, null);
+                    tempScore = tempScore/10;
+                    scoreX -= 15;
+                }
+                timeDonVi = dem % 10;
+                timeChuc = dem / 10;
+                bufferGraphic2D.drawImage(listDiem.get(timeChuc), 685, 50, null);
+                bufferGraphic2D.drawImage(listDiem.get(timeDonVi), 700, 50, null);
 		for (Gold gold : listGold) {
 			gold.draw(bufferedImage2);
 		}
@@ -161,6 +214,9 @@ public class Game_Screen implements Screen {
 		}
 		for (Tnt tnt : listTnt) {
 			tnt.draw(bufferedImage2);
+		}
+                for (kim_cuong kc : listKc) {
+			kc.draw(bufferedImage2);
 		}
 		g2d.drawImage(bufferedImage2, 0, 0, null);
 	}
